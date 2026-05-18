c71902069c86773744812fcc7788c5de24aeeeae
NguyenQuangNhan - 2177 <quangnhan191004@gmail.com>
2026-05-18 13:08:40 +0700

    feat(auth): Refactor API endpoints and router structure

--- src/api/auth/me.ts
+++ src/api/auth/me.ts
@@ -1,22 +1,12 @@
 import axiosClient from "../axiosClient";
 import type { User } from "../../types/auth";
-
-interface MeResponse {
-  message: string;
-  data: {
-    user_id: number;
-    username: string;
-    email: string;
-    full_name: string;
-    roles: string[];
-  };
-}
+import type { MeResponse } from "../../types/login";
 
 export const getMe = async (): Promise<User> => {
-  const response = await axiosClient.get<any, MeResponse>("/me");
+  const response = await axiosClient.get<any, MeResponse>("/user-info-by-token");
   const data = response.data;
   return {
-    id: data.user_id,
+    id: data.id,
     username: data.username,
     email: data.email,
     full_name: data.full_name,
